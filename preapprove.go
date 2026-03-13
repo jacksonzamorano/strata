6f7abe43b67a6d1950376639b715366b15aea3b5
Jackson Zamorano <jackson@jacksonzamorano.com>
2026-03-13 07:14:21 -0600

    fix: pre-approve permission now uses dep name

--- preapprove.go
+++ preapprove.go
@@ -2,17 +2,17 @@ package strata
 
 import "github.com/jacksonzamorano/strata/core"
 
-func AllowAll(cmp core.ComponentManifest, pm core.PermissionAction) core.Permission {
+func AllowAll(nm string, pm core.PermissionAction) core.Permission {
 	return core.Permission{
-		Container: cmp.Name,
+		Container: nm,
 		Action:    pm,
 		Scope:     "*",
 	}
 }
 
-func AllowOne(cmp core.ComponentManifest, pm core.PermissionAction, scope string) core.Permission {
+func AllowOne(nm string, pm core.PermissionAction, scope string) core.Permission {
 	return core.Permission{
-		Container: cmp.Name,
+		Container: nm,
 		Action:    pm,
 		Scope:     scope,
 	}
